a1c77f459a4ea312d12261b20f12c9f5f64151cc
ANGELE Brice <angele.bricep@gmail.com>
2021-04-03 18:25:43 +0200

    Crud USER

---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const authController = require('../controllers/auth.controller');
+const userController = require('../controllers/user.controller');
+
+//auth
+router.post('/register', authController.signUp);
+
+//user DB
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.userInfo);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+//router.patch('/waitlist/:id', userController.addWaitlist); ---- À vérifier ----
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const app = express();
 
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+//routes
+app.use('/api/user', userRoutes);
+
+// server
 app.listen(process.env.PORT, () => {
     console.log(`listening on port : ${process.env.PORT}`);
 })
