3f8cee5bc9da4ea38109949cc6b8c7c24d0c3e35
ANGELE Brice <angele.bricep@gmail.com>
2021-04-13 14:28:52 +0200

    end back V1.0.0

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const cityRoutes = require('./routes/city.routes');
 const teamRoutes = require('./routes/team.routes');
-require('dotenv').config({path: './config/.env'});
+require('dotenv').config({ path: './config/.env' });
 require('./config/db');
 const { checkUser, requireAuth } = require('./middleware/auth.middleware');
+
+const cors = require('cors');
+
 const app = express();
 
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}
+app.use(cors(corsOptions));
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
